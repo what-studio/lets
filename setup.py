21d795ae4d83078561323ebde9aff2dddbe139dc
Heungsub Lee <sub@subl.ee>
2013-10-16 01:10:34 +0900

    remove classifiers for Python 2.5 and 2.6

--- setup.py
+++ setup.py
@@ -87,8 +87,6 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.5',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: Implementation :: CPython',
                  'Topic :: Software Development'],

065506a2dab6cf3876eda9954f0777a93c586ae4
Heungsub Lee <sub@subl.ee>
2016-09-07 23:03:22 +0900

    Aim only stable gevent versions

--- setup.py
+++ setup.py
@@ -150,7 +150,7 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: Implementation :: CPython',
                  'Topic :: Software Development'],
-    install_requires=['gevent>=1.1a2', 'gipc'],
+    install_requires=['gevent>=1.1.0', 'gipc'],
     tests_require=['psutil', 'pytest', 'pytest-rerunfailures'],
     test_suite='...',
 )

18eaa5da66486b055363ece20e37b72fa6e61dad
Heungsub Lee <sub@subl.ee>
2016-09-13 03:15:05 +0900

    Normalize implementation of Earliest as Alarm

--- lets/__init__.py
+++ lets/__init__.py
@@ -14,8 +14,8 @@
 from __future__ import absolute_import
 
 from .__about__ import __version__  # noqa
+from .alarm import Alarm, Earliest, Latest
 from .atomicity import atomic
-from .earliest import Earliest
 from .jobqueue import JobQueue
 from .objectpool import ObjectPool
 from .processlet import ProcessExit, Processlet, ProcessLocal, ProcessPool
@@ -26,8 +26,8 @@ from .slavery import (
 from .transparentlet import TransparentGroup, Transparentlet
 
 
-__all__ = ['atomic', 'Earliest', 'JobQueue', 'join_slaves', 'link_partner',
-           'link_slave', 'MasterGreenletExit', 'ObjectPool', 'Processlet',
-           'ProcessLocal', 'ProcessExit', 'ProcessPool', 'Quietlet',
-           'spawn_partner', 'spawn_partner_later', 'spawn_slave',
+__all__ = ['Alarm', 'atomic', 'Earliest', 'JobQueue', 'join_slaves', 'Latest',
+           'link_partner', 'link_slave', 'MasterGreenletExit', 'ObjectPool',
+           'Processlet', 'ProcessLocal', 'ProcessExit', 'ProcessPool',
+           'Quietlet', 'spawn_partner', 'spawn_partner_later', 'spawn_slave',
            'spawn_slave_later', 'Transparentlet', 'TransparentGroup']
--- lets/alarm.py
+++ lets/alarm.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-   lets.earliest
-   ~~~~~~~~~~~~~
+   lets.alarm
+   ~~~~~~~~~~
 
    An event which is awoken up based on time.
 
@@ -9,35 +9,41 @@
    :license: BSD, see LICENSE for more details.
 
 """
+import operator
 from time import time as now
 
 from gevent import get_hub, Timeout
 from gevent.event import Event
 
 
-__all__ = ['Earliest']
+__all__ = ['Alarm', 'Earliest', 'Latest']
 
 
-class Earliest(object):
-    """A :class:`gevent.event.AsyncResult`-like class to wait until the
-    earliest time among many times that've been set.  So you can set many times
-    but it will be wakened up just once.
+class _Alarm(object):
+    """A :class:`gevent.event.AsyncResult`-like class to wait until the final
+    set time.
     """
 
     __slots__ = ('time', 'value', 'timer', 'event')
+
+    #: Implement it to decide to reschedule the awaking time.  It's a function
+    #: which takes 2 time arguments.  The first argument is new time to set,
+    #: and the second argument is the previously accepted time.  Both arguments
+    #: are never ``None``.
+    accept = NotImplemented
 
     def __init__(self):
         self.time = self.value = self.timer = None
         self.event = Event()
 
     def set(self, time, value=None):
-        """Sets the time to awake up.  If the time is later than the previously
-        given time, will be ignored and it returns ``False``.
+        """Sets the time to awake up.  If the time is not accepted, will be
+        ignored and it returns ``False``.  Otherwise, returns ``True``.
         """
         if time is None:
             raise TypeError('use clear() instead of setting none time')
-        if self.time is not None and self.time <= time:
-            # Later time given.
+        elif self.time is not None and not self.accept(time, self.time):
+            # Not accepted.
             return False
         self._reset(time, value)
         delay = time - now()
@@ -50,17 +56,21 @@ class Earliest(object):
             self.event.set()
         return True
 
+    def when(self):
+        """When it will be awoken or ``None``."""
+        return self.time
+
     def ready(self):
         """Whether it has been awoken."""
         return self.event.ready()
 
     def wait(self, timeout=None):
-        """Waits until the earliest awaking time.  It returns the time."""
+        """Waits until the awaking time.  It returns the time."""
         if self.event.wait(timeout):
             return self.time
 
     def get(self, block=True, timeout=None):
-        """Waits and gets the earliest awaking time and the value."""
+        """Waits until and gets the awaking time and the value."""
         if not block and not self.ready():
             raise Timeout
         if self.event.wait(timeout):
@@ -80,3 +90,26 @@ class Earliest(object):
 
     def __nonzero__(self):
         return self.time is not None
+
+
+class Alarm(_Alarm):
+    """An alarm which accepts any time.  Its awaking time will always be reset.
+    """
+
+    accept = lambda x, y: True
+
+
+class Earliest(_Alarm):
+    """An alarm which accepts only the earliest time among many times that've
+    been set.  Earlier time means that smaller timestamp.
+    """
+
+    accept = operator.lt
+
+
+class Latest(_Alarm):
+    """An alarm which accepts only the latest time among many times that've
+    been set.  Later time means that bigger timestamp.
+    """
+
+    accept = operator.gt

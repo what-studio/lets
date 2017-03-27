09d38f2476674f738812d3adaa212d22b96619c8
Heungsub Lee <sub@subl.ee>
2017-03-27 18:31:40 +0900

    Expose lets.pipe()

--- lets/__init__.py
+++ lets/__init__.py
@@ -18,7 +18,8 @@ from lets.alarm import Alarm, Earliest, Latest
 from lets.atomicity import atomic
 from lets.jobqueue import JobQueue
 from lets.objectpool import ObjectPool
-from lets.processlet import ProcessExit, Processlet, ProcessLocal, ProcessPool
+from lets.processlet import (
+    pipe, ProcessExit, Processlet, ProcessLocal, ProcessPool)
 from lets.quietlet import Quietlet
 from lets.slavery import (
     join_slaves, link_partner, link_slave, MasterGreenletExit, spawn_partner,
@@ -28,6 +29,6 @@ from lets.transparentlet import TransparentGroup, Transparentlet
 
 __all__ = ['Alarm', 'atomic', 'Earliest', 'JobQueue', 'join_slaves', 'Latest',
            'link_partner', 'link_slave', 'MasterGreenletExit', 'ObjectPool',
-           'Processlet', 'ProcessLocal', 'ProcessExit', 'ProcessPool',
+           'pipe', 'Processlet', 'ProcessLocal', 'ProcessExit', 'ProcessPool',
            'Quietlet', 'spawn_partner', 'spawn_partner_later', 'spawn_slave',
            'spawn_slave_later', 'Transparentlet', 'TransparentGroup']
